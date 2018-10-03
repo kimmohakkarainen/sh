69e60d4dcab5e02f25937e7d505bb39b76303d29
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2018-10-03 19:55:24 +0300

    clean login-page

--- src/loginview.js
+++ src/loginview.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  Jumbotron,
-  FormGroup,
-  ControlLabel,
-  FormControl,
-  Button
-} from "react-bootstrap";
+import { FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { fetchState } from "./actions";
 
@@ -25,30 +19,34 @@ class LoginView extends Component {
 
   render() {
     return (
-      <Jumbotron>
-        <h1>Login</h1>
-        <FormGroup>
-          <ControlLabel>username</ControlLabel>
-          <FormControl
-            type="text"
-            placeholder="username"
-            value={this.state.username}
-            onChange={e => this.setState({ username: e.target.value })}
-          />
-        </FormGroup>
-        <FormGroup>
-          <ControlLabel>password</ControlLabel>
-          <FormControl
-            type="password"
-            placeholder="password"
-            value={this.state.password}
-            onChange={e => this.setState({ password: e.target.value })}
-          />
-        </FormGroup>
-        <Button onClick={this.login} bsStyle="primary">
-          Login
-        </Button>
-      </Jumbotron>
+      <div className="login-page">
+        <div className="login-form">
+          <h1>
+            Tiedot<sup>2</sup>
+          </h1>
+          <FormGroup>
+            <ControlLabel>username</ControlLabel>
+            <FormControl
+              type="text"
+              placeholder="username"
+              value={this.state.username}
+              onChange={e => this.setState({ username: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>password</ControlLabel>
+            <FormControl
+              type="password"
+              placeholder="password"
+              value={this.state.password}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </FormGroup>
+          <Button onClick={this.login} bsStyle="primary">
+            Login
+          </Button>
+        </div>
+      </div>
     );
   }
 }
